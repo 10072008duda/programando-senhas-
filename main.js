9487ecec7a23048eb98d7633dd8fdcfefa2cea24
10072008duda <s.jeniffer10@escola.pr.gov.br>
2026-08-04 13:58:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -187,3 +187,4 @@ const checkbox = document.querySelectorAll('.checkbox');const forcaSenha = docum
 }<p class="entropia"></p>const valorEntropia = document.querySelector('.entropia');const valorEntropia = document.querySelector('.entropia');
 valorEntropia.textContent = 2**Math.floor(entropia);const valorEntropia = document.querySelector('.entropia');
 valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24));valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
